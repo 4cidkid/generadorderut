7b9e4815c6d6d183d4d6318e936f56b5643f2339
Nicolas Torres <desmondgamesyt@gmail.com>
2023-02-13 01:17:57 -0300

    function to generate a valid random rut now is on other file to better practice

--- src/Components/Rutgen/Rutgen.js
+++ src/Components/Rutgen/Rutgen.js
@@ -1,72 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Rutgen.css";
-
+import GenerateRandomRut from './function'
 export default function Rutgen() {
+    const[state,setState] = useState(false)
+    function handleClick(){
+        setState(!state)
+    }
   return (
     <div>
       <h1>RUT GENERATOR</h1>
-      <button onClick={generateRandomRut}>Generame!</button>
+      <button onClick={handleClick} style={{backgroundColor}}>Generame!</button>
+      {state ? <GenerateRandomRut /> : <GenerateRandomRut/>}
+      {state ? <GenerateRandomRut /> : <GenerateRandomRut/>}
+      {state ? <GenerateRandomRut /> : <GenerateRandomRut/>}
+      {state ? <GenerateRandomRut /> : <GenerateRandomRut/>}
+      {state ? <GenerateRandomRut /> : <GenerateRandomRut/>}
+      {state ? <GenerateRandomRut /> : <GenerateRandomRut/>}
+      {state ? <GenerateRandomRut /> : <GenerateRandomRut/>}
+      {state ? <GenerateRandomRut /> : <GenerateRandomRut/>}
+      {state ? <GenerateRandomRut /> : <GenerateRandomRut/>}
+      {state ? <GenerateRandomRut /> : <GenerateRandomRut/>}
+      {state ? <GenerateRandomRut /> : <GenerateRandomRut/>}
     </div>
   );
 }
-
-function generateRandomRut() {
-  let newRut = Math.floor(Math.random() * (40000000 - 1000000 + 1) + 1000000);
-  if (validateRandomRut(newRut) == false) {
-    generateRandomRut();
-  } else {
-    return elRut;
-  }
-}
-
-function validateRandomRut(rut) {
-  let rutToValidate = rut;
-  let reverseRut = [];
-  rutToValidate = rutToValidate.toString();
-  rutToValidate = rutToValidate.split("");
-  for (let i = rutToValidate.length - 1; i >= 0; i--) {
-    reverseRut.push(rutToValidate[i]);
-  }
-  let multipliedRut = [];
-  let j = 2;
-  let lastIndex = reverseRut.lastIndexOf(reverseRut[reverseRut.length - 1]);
-  for (i of reverseRut) {
-    if (lastIndex + 1 == reverseRut.lastIndexOf(i)) {
-      break;
-    }
-    for (j; j <= 7; j++) {
-      multipliedRut.push(i * j);
-      if (j == 7) {
-        j = 2;
-      } else {
-        j = j + 1;
-      }
-      break;
-    }
-  }
-  let sumateRut = 0
-  for(let i=0;i<multipliedRut.length;i++){
-    sumateRut += multipliedRut[i]
-  }
-  let newSumateRut = Math.floor(sumateRut/11)
-  newSumateRut = newSumateRut*11
-  sumateRut = sumateRut - newSumateRut;
-  if(sumateRut<0){
-    sumateRut = sumateRut*-1
-  }
-  let finalResult = 11-sumateRut
-  if(finalResult == 11){
-    finalResult = 0
-  }
-  if(finalResult == 10){
-    finalResult = 'k'
-  }
-  if(finalResult <= 11 || finalResult == 'k'){
-    elRut = `${rut}-${finalResult}`
-    console.log(elRut)
-    return true
-  }else{
-    return false
-  }
-}
-let elRut = 0;
